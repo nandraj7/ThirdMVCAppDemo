e66c50ff427ffbe6f2da43aa748076dc23b4931f
hp <hp@HPELITEBOOK840G>
2023-04-05 23:32:43 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThirdMVCAppDemo.Models;
+
+namespace ThirdMVCAppDemo.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            SecondMvcappDemoContext db = new SecondMvcappDemoContext();
+            var users = db.Users.ToList();
+
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            SecondMvcappDemoContext db = new SecondMvcappDemoContext();
+            db.Users.Add(user);
+            db.SaveChanges();
+
+
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/SecondMvcappDemoContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ThirdMVCAppDemo.Models;
+
+public partial class SecondMvcappDemoContext : DbContext
+{
+    public SecondMvcappDemoContext()
+    {
+    }
+
+    public SecondMvcappDemoContext(DbContextOptions<SecondMvcappDemoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("server=localhost\\SQLEXPRESS; database=SecondMVCAppDemo; trusted_connection=true; trustservercertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC070F01C3E9");
+
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
